6c4aba22a6557d472680b8777a624750784a5133
Duskitty <theduskitty@gmail.com>
2020-08-06 15:44:58 -0500

    Updated to 1.16.1

--- src/main/java/io/github/duskitty/glowingbeds/Main.java
+++ src/main/java/io/github/duskitty/glowingbeds/Main.java
@@ -2,7 +2,7 @@ package io.github.duskitty.glowingbeds;
 
 import net.fabricmc.api.ModInitializer;
 
-public class Reference implements ModInitializer {
+public class Main implements ModInitializer {
 	@Override
 	public void onInitialize() {
 	}
--- src/main/java/io/github/duskitty/glowingbeds/mixin/PlayerEntityMixin.java
+++ src/main/java/io/github/duskitty/glowingbeds/mixin/PlayerEntityMixin.java
@@ -5,9 +5,11 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -15,19 +17,22 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.List;
 
-@Mixin(PlayerEntity.class)
-public class PlayerEntityMixin {
+@Mixin(ServerPlayerEntity.class)
+public abstract class PlayerEntityMixin {
+
+    @Shadow public abstract boolean isCreative();
 
     public PlayerEntityMixin() {
         super();
     }
 
-    @Inject(method = "trySleep", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getEntities(Ljava/lang/Class;Lnet/minecraft/util/math/Box;Ljava/util/function/Predicate;)Ljava/util/List;"), locals = LocalCapture.CAPTURE_FAILHARD)
-    private void getEntity(BlockPos pos, CallbackInfoReturnable<Either> cir, double d, double e) {
+    @Inject(method = "trySleep", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;isCreative()Z"), locals = LocalCapture.CAPTURE_FAILHARD)
+    private void getEntity(BlockPos pos, CallbackInfoReturnable<Either> cir) {
+        if (this.isCreative() == false){
         List<HostileEntity> list2 = ((PlayerEntity)(Object) this).world.getEntities(HostileEntity.class, new Box((double)pos.getX() - 8.0D, (double)pos.getY() - 5.0D, (double)pos.getZ() - 8.0D, (double)pos.getX() + 8.0D, (double)pos.getY() + 5.0D, (double)pos.getZ() + 8.0D), (hostileEntity) -> {
            hostileEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.GLOWING,60,1) );
            return hostileEntity.isAngryAt(((PlayerEntity)(Object) this));
-        });
+        });}
     }
 
 
